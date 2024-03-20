20508d6d6da0a524904b6cdbb92b3d121e20ffa6
Julio Sanchez <julio239995@gmail.com>
2024-03-19 19:33:30 -0600

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			animation: {
+			  text: 'text 5s ease infinite',
+			},
+			keyframes: {
+			  text: {
+				'0%, 100%': {
+				  'background-size': '200% 200%',
+				  'background-position': 'left center',
+				},
+				'50%': {
+				  'background-size': '200% 200%',
+				  'background-position': 'right center',
+				},
+			  },
+			},
+		},
+	},
+	plugins: [],
+}
